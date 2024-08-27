57164c2625da87373d496d37d8ffd7258e3a09d7
sxntiagoad <salvarezd3@eafit.edu.co>
2024-08-26 21:56:26 -0500

    fixed error

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,62 +1,56 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import { createAccesToken } from "../libs/jwt.js";
-
+import { createAccessToken } from "../libs/jwt.js";
 export const register = async (req, res) => {
-    const { username, email, password } = req.body;
-    try {
-        const passwordHash = await bcrypt.hash(password, 10);
-        const newUser = new User({
+    const { username, email, password } = req.body; //get user data from request
+    try { //try to register new user
+
+        const passwordHash = await bcrypt.hash(password, 10); //encrypt password
+        const newUser = new User({ //create new user
             username,
             email,
             password: passwordHash,
         });
 
-        const userSaved = await newUser.save();
-        const token = await createAccesToken({ id: userSaved._id });
+        const userSaved=await newUser.save(); //save user to database
+        const token = createAccessToken({ id: userSaved._id }); //create token
+        
+        res.cookie('token', token); //set cookie
+        res.json({id:userSaved._id,  //return user data to client
+            username:userSaved.username,
+            email:userSaved.email, 
+            createdAt:userSaved.createdAt,
+            updatedAt:userSaved.updatedAt,
+        }); //return user data without password to client
 
-        res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
-        res.json({
-            id: userSaved._id,
-            username: userSaved.username,
-            email: userSaved.email,
-            createdAt: userSaved.createdAt,
-            updatedAt: userSaved.updatedAt,
-        });
-
-    } catch (error) {
-        res.status(500).send({ message: "error.message" });
+    } catch (error) { //catch any errors
+        console.log(error);
+        res.status(500).send({ message: "Error registering user" }); //return error message
     }
-};
+}; //register new user
 
-export const login =async (req, res) => {
-    const { email, password } = req.body;
+export const login = async (req, res) => {
+    const { email, password } = req.body; //get user data from request
+  
     try {
-
-        const userFound = await User.findOne({ email });    
-        if (!userFound) return res.status(400).json({ message: "User not found" }); 
-
-        const isMatch = await bcrypt.compare(password, userFound.password);     
-        if (!isMatch) return res.status(400).json({ message: "Incorrect password" });
-        const newUser = new User({
-            username,
-            email,
-            password: passwordHash,
-        });
-
-        const userSaved = await newUser.save(); 
-        const token = await createAccesToken({ id: userFound._id }); 
-
-        res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
-        res.json({
-            id: userSaved._id,
-            username: userSaved.username,
-            email: userSaved.email,
-            createdAt: userSaved.createdAt,
-            updatedAt: userSaved.updatedAt,
-        });
-
+      const userFound = await User.findOne({ email }); //find user by email
+      if (!userFound) return res.status(400).json({ message: "User not found" });   //return error if user not found
+  
+      const isMatch = await bcrypt.compare(password, userFound.password); //compare password
+      if (!isMatch)
+        return res.status(400).json({ message: "Incorrect password" }); //return error if password is incorrect
+  
+      const token = await createAccessToken({ id: userFound._id }); //create token
+  
+      res.cookie("token", token); //set cookie 
+      res.json({ //return user data to client
+        id: userFound._id, 
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+      });
     } catch (error) {
-        res.status(500).send({ message: "error.message" });
+      res.status(500).json({ message: error.message }); //return error message
     }
-};
+  };
--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
-export function createAccesToken(payload) {
+export function createAccessToken(payload) {
     return new Promise((resolve, reject) => {
         jwt.sign(
             payload,
-            TOKEN_SECRET,
-            { expiresIn: '1d' },
+            TOKEN_SECRET,   
+            { expiresIn: '1d' },    
             (err, token) => {
-                if (err) reject(err);
+                if (err) reject(err); 
                 resolve(token);
             }
         );
