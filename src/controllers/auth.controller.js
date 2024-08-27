40d4967cce8bde3648c9c1a41d70e5a828796902
sxntiagoad <salvarezd3@eafit.edu.co>
2024-08-26 19:07:17 -0500

    Update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,29 @@
-export const register = (req, res) => {
-    const { username, email, password } = req.body;
-    console.log(username, email, password);
-}
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+export const register = async (req, res) => {
+    const { username, email, password } = req.body; //get user data from request
+    try { //try to register new user
 
-export const login = (req, res) => res.send("login");
+        const passwordHash = await bcrypt.hash(password, 10); //encrypt password
+        const newUser = new User({ //create new user
+            username,
+            email,
+            password: passwordHash,
+        });
+
+        const userSaved=await newUser.save(); //save user to database
+        
+        res.json({id:userSaved._id,
+            username:userSaved.username,
+            email:userSaved.email, 
+            createdAt:userSaved.createdAt,
+            updatedAt:userSaved.updatedAt,
+        }); //return user data without password to client
+
+    } catch (error) { //catch any errors
+        console.log(error);
+        res.status(500).send({ message: "Error registering user" }); //return error message
+    }
+}; //register new user
+
+export const login = (req, res) => res.send("login"); 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  username: {
+const userSchema = new mongoose.Schema({  // Create a schema
+  username: { // Define the properties of the schema 
     type: String,
     required: true,
-    trim: true,
+    trim: true, // Remove whitespace from both ends of a string
   },
   email: {
     type: String,
     required: true,
     trim: true,
-    unique: true,
+    unique: true, // Ensure that the email is unique
   },
   password: {
     type: String,
     required: true,
   },
-});
-export default mongoose.model("User", userSchema);
+}, { timestamps: true });   // Add timestamps to the schema
+export default mongoose.model("User", userSchema); // Create a model from the schema
