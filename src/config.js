ca5a0b8861c7a3652781f05c23ac1315c2a92677
sxntiagoad <salvarezd3@eafit.edu.co>
2024-09-25 17:45:44 -0500

    Guardar cambios locales en config.js y s3config.js

--- src/config.js
+++ src/config.js
@@ -7,11 +7,5 @@ export const API_KEY = "7cd6153d-f3a0-413a-8211-a94b8387f19e:e9944f0417b0115a1e8
 export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 export const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
 export const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
-export const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION ?? 'us-east-2';
-
-// Solo para depuración, no incluyas esto en producción
-console.log('AWS_BUCKET_NAME:', AWS_BUCKET_NAME);
-console.log('AWS_BUCKET_REGION:', AWS_BUCKET_REGION);
-console.log('AWS_ACCESS_KEY definida:', !!AWS_ACCESS_KEY);
-console.log('AWS_SECRET_KEY definida:', !!AWS_SECRET_KEY);
+export const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION;
 
--- src/s3config.js
+++ src/s3config.js
@@ -2,9 +2,6 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { Upload } from "@aws-sdk/lib-storage";
 import { AWS_BUCKET_NAME, AWS_BUCKET_REGION, AWS_ACCESS_KEY, AWS_SECRET_KEY } from './config.js';
 
-console.log('AWS_BUCKET_NAME en s3config:', AWS_BUCKET_NAME); // Añade este log para depuración
-console.log('AWS_BUCKET_REGION en s3config:', AWS_BUCKET_REGION);
-
 const s3Client = new S3Client({
   region: AWS_BUCKET_REGION,
   credentials: {
@@ -14,10 +11,6 @@ const s3Client = new S3Client({
 });
 
 const uploadToS3 = async (buffer, fileName, contentType) => {
-  console.log('Iniciando uploadToS3');
-  console.log('Bucket:', AWS_BUCKET_NAME);
-  console.log('Region:', AWS_BUCKET_REGION);
-  console.log('FileName:', fileName);
   
   if (!AWS_BUCKET_NAME) {
     throw new Error('AWS_BUCKET_NAME is not defined');
